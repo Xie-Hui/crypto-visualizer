37b6423a8eae0ab8ec40c5447a35f02882096748
Hui Xie <polarcracker@gmail.com>
2019-09-19 23:08:05 -0700

    added basic styles structure

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,9 +1,54 @@
 import React, { Fragment } from 'react';
-import { CssBaseline } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
 
-export default (props) => (
-    <Fragment>
-        <CssBaseline />
-        <div className='layout'>{props.children}</div>
-    </Fragment>
-);
+//css normalizer
+import 'normalize.css';
+
+const useStyles = makeStyles((theme) => ({
+    '@global': {
+        html: {
+            boxSizing: 'border-box'
+        },
+
+        '*, *:before, *:after': {
+            boxSizing: 'inherit'
+        },
+
+        'html, body': {
+            minHeight: '100vh'
+        },
+
+        body: {
+            display: 'flex',
+            margin: 0,
+            padding: 0,
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: theme.color.bg,
+            color: theme.color.text,
+            fontFamily: ['Roboto Mono', 'monospace'],
+            fontWeight: 400,
+            fontSize: 14,
+            '-moz-osx-font-smoothing': 'grayscale',
+            '-webkit-font-smoothing': 'antialiased'
+        },
+
+        '#root': {
+            height: '100%',
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            flex: [1, 1, '100%']
+        }
+    }
+}));
+
+export default (props) => {
+    const globalStyles = useStyles();
+    return (
+        <Fragment>
+            <div className={globalStyles}>{props.children}</div>
+        </Fragment>
+    );
+};
